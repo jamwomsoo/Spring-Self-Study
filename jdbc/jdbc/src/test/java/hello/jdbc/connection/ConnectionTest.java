b4bff1e4e6563a777516bf2ab3b52f08e369639d
jamwomsoo <tjdtn9692@gmail.com>
2022-05-03 22:22:36 +0900

    data source connection2"

--- jdbc/jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ jdbc/jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -1,6 +1,7 @@
 package hello.jdbc.connection;
 
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -31,6 +32,21 @@ public class ConnectionTest {
 
         useDataSource(dataSource);
     }
+
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        HikariDataSource dataSource = new HikariDataSource();
+        // 커넥션 풀링
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("myName");
+
+        useDataSource(dataSource);
+        Thread.sleep(10000);
+    }
+
 
     private void useDataSource(DataSource dataSource) throws SQLException {
         // 커넥션을 획득할때는 단순히 getConnection을 호출만함
