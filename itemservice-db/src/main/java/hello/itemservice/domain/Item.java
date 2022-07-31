765a48873587e6dba91caa32130772c852deeb43
jamwomsoo <tjdtn9692@gmail.com>
2022-07-31 18:21:40 +0900

    db final commit

--- itemservice-db/src/main/java/hello/itemservice/domain/Item.java
+++ itemservice-db/src/main/java/hello/itemservice/domain/Item.java
@@ -2,13 +2,20 @@ package hello.itemservice.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
+
 @Data
+@Entity
 public class Item {
 
+    // DB에서 값을 증가시켜줌
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "item_name", length = 10)
     private String itemName;
-    private Integer price;
+    private Integer price;     // 테이블 컬럼명이랑 같으면 생략가능
     private Integer quantity;
 
     public Item() {
