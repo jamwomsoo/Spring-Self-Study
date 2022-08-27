96e9c2eeb76f933463e2b3dc45236f26db5de513
jamwomsoo <tjdtn9692@gmail.com>
2022-08-27 21:45:09 +0900

    commit data

---
+++ springtx/src/main/java/hello/springtxt/SpringtxtApplication.java
@@ -0,0 +1,13 @@
+package hello.springtxt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtxtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringtxtApplication.class, args);
+	}
+
+}
